2612c80f9e96cfbd8a8942e9c87bab06c5c39c50
Matheus Rodrigues <matheusdesouzarodrigues34@gmail.com>
2026-02-27 12:38:04 -0300

    Initialize InspiraAgoraApplication with Spring Boot

---
+++ src/main/java/com/inspiraagora/app/InspiraAgoraApplication.java
@@ -0,0 +1,12 @@
+package com.inspiraagora.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InspiraAgoraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InspiraAgoraApplication.class, args);
+    }
+}
