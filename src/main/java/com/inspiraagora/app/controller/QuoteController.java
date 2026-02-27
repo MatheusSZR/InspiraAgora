f298b88082e8280a37c18157457cee49f9a69ebd
Matheus Rodrigues <matheusdesouzarodrigues34@gmail.com>
2026-02-27 12:40:11 -0300

    Implement QuoteController for random quotes

---
+++ src/main/java/com/inspiraagora/app/controller/QuoteController.java
@@ -0,0 +1,23 @@
+package com.inspiraagora.app.controller;
+
+import com.inspiraagora.app.model.Quote;
+import com.inspiraagora.app.service.QuoteService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/quotes")
+public class QuoteController {
+
+    private final QuoteService quoteService;
+
+    public QuoteController(QuoteService quoteService) {
+        this.quoteService = quoteService;
+    }
+
+    @GetMapping("/random")
+    public Quote getRandomQuote() {
+        return quoteService.getRandomQuote();
+    }
+}
